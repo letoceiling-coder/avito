4c5ab0441205fdd7a5907b24182d4b77d624c187
letoceiling-coder <dsc-23@yandex.ru>
2025-11-25 18:38:55 +0300

    Add built frontend files and OAuth fixes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <title>{{ config('app.name', 'Laravel') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script>
